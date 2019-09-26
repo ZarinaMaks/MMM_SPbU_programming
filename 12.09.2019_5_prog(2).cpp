905af9a5168cfe0464929deae809e518d26725ba
ZarinaMaks <55331028+ZarinaMaks@users.noreply.github.com>
2019-09-26 03:10:39 +0300

    Add files via upload

---
+++ 12.09.2019_5_prog(2).cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void rplc(int* arr, int n){
+    int i, j = n - 1;
+    for(i = 0; i < n; i++){
+        if(i == j){
+            if(arr[i] % 2 == 1)
+                arr[i] = 0;
+            break;
+        }
+        if(arr[i] % 2 == 1){
+            while(arr[j] % 2 == 1){
+                arr[j] = 0;
+                j--;
+            }
+            arr[i] = arr[j];
+            arr[j] = 0;
+            j--;
+        }
+    }
+}
+
+int main() {
+    int n, i;
+    cin >>n;
+    int *a = new int[n];
+    for(i = 0; i < n; i++)
+        cin >>a[i];
+
+
+    rplc(a, n);
+    for(i = 0; i < n; i++)
+        cout << a[i] << " ";
+
+    return 0;
+}
