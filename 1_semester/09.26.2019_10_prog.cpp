78a3f754c7fad990e439233b05b09c991068b5d2
ZarinaMaks <55331028+ZarinaMaks@users.noreply.github.com>
2019-11-06 01:45:17 +0300

    Update 09.26.2019_10_prog.cpp

--- 1_semester/09.26.2019_10_prog.cpp
+++ 1_semester/09.26.2019_10_prog.cpp
@@ -1,15 +1,30 @@
 //Напишите программу перебирающую все перестановки массива букв в лексикографическом порядке. Программа должна работать не более чем за O(n!*n) шагов. 
-#include <algorithm>
-#include <string>
 #include <iostream>
+#include <set>
+#include <string>
+#include <cstring>
 using namespace std;
- 
+
 int main()
 {
-    string s = "cadb";
-    sort(s.begin(), s.end());
-    do
-    {
-        cout << s << endl;
-    } while(next_permutation(s.begin(), s.end()));
+   char chars[] = "reowfdsgba";
+   int size = sizeof(chars) / sizeof(chars[0]) - 1; 
+   char temp; 
+   int count = 0;
+   // bubble sort
+   for (int i = 0; i < size - 1; i++) {
+      for (int j = 0; j < size - i - 1; j++) {
+         if (chars[j] > chars[j + 1]) {
+            temp = chars[j];
+            chars[j] = chars[j + 1];
+            chars[j + 1] = temp;
+            ++count;
+         }
+      }
+   }
+   for (int i = 0; i < size; i++) {
+      cout << chars[i] << " ";
+   }
+   cout << endl;
+   return 0;
 }
