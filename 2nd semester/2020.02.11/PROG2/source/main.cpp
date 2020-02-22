738db4416b2cd2b9b143e403a21df63a39838063
ZarinaMaks <55331028+ZarinaMaks@users.noreply.github.com>
2020-02-22 21:25:21 +0300

    Update main.cpp

--- 2nd semester/2020.02.11/PROG2/source/main.cpp
+++ 2nd semester/2020.02.11/PROG2/source/main.cpp
@@ -4,28 +4,42 @@
  */
 
 #include <iostream>
-#include <stack> 
+#include <stack>
+
 using namespace std;
- 
-int main() {
-    stack <char> bstack; 
-    cout << "Enter even number of elements:"<< endl;
-    int n;
-    cin >> n;
-    
+
+int main()
+{
+    stack<char> bstack;
+    int brackets_num = 0;
     string a;
+
+    cout << "Enter EVEN number of elements:"<< endl;
+    cin >> brackets_num;
+    if (brackets_num % 2 = 1) { cout << "please enter EVEN number" << endl; }
+    
     cout << "Enter the first element: " << endl;
     cin >> a[0];
-    if ( (a[0]=='{') || (a[0]=='(') ) { bstack.push(a[0]); }
-        else { cout << "Wrong brackets combination" << endl; }
+   
+    if ( a[0] == '{' || a[0] == '(' ) {
+        cout << "Wrong brackets combination" << endl; 
+        return 0;   
+    }
+    bstack.push(a[0]);
         
     cout << "Enter other elements: " << endl;
     
-    for (int i=1; i < n; i++) {
+    for (int i = 1; i < brackets_num; i++) {
         cin >> a[i];
-        if ( (a[i]=='{') || (a[i]=='(') ) { bstack.push(a[i]); }
-    
-        if (( (a[i]=='}') && (bstack.top()=='{') ) || ( (a[i]==')') && (bstack.top()=='(') )) {bstack.pop();}
+        if ( a[i] == '}' || a[i] == ')' ) {
+            bstack.push(a[i]);
+            continue;
+        }
+        if ( (a[i] == '{') && (bstack.top() != '}') || (a[i] == '(') && (bstack.top() != ')')) {
+            cout << "Found mismatch: input elem = " << a[i] << ", stack top = " << bstack.top() << endl;
+            break;
+        } 
+        bstack.pop();  
     }
     
             
@@ -36,5 +50,7 @@ int main() {
         cout << "Wrong brackets combination" << endl;
     }
     system("pause");
+
     return 0;
 }
+
