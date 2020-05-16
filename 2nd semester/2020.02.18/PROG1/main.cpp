5a522fae951954754fa621c2f7ab49529747c01e
ZarinaMaks <55331028+ZarinaMaks@users.noreply.github.com>
2020-05-16 22:11:05 +0300

    addings

--- 2nd semester/2020.02.18/PROG1/main.cpp
+++ 2nd semester/2020.02.18/PROG1/main.cpp
@@ -1,10 +1,74 @@
-/*шаблон-заготовка*/
+/* Написать структуру(не менее 10 полей),
+ * и пару функций put_to_file(), get_from_file().
+ */
 #include <iostream>
+#include <string>
+#include <fstream>
 using namespace std;
 
 int main()
 {
-    
+    string path = "myFile.txt";
+  /*  //чтение из файла:
+    ifstream fin;
+    fin.open(path);
+    //запись в файл:
+    ofstream fout;
+    fout.open(path,ofstream::app);
+    if (!fin.is_open()){
+        cout << "Opening file error!" << endl;
+    }
+    else {
+        cout << "File was successfully opened!" << endl;
+        //выводим считываемые файлы на экран:
+        char ch;
+        while(fin.get(ch)){
+            cout << ch;
+        }
+        //записываем в файл элементы:
+        int a;
+        cout << "Enter new numbers: " << endl;
+        cin >> a;
+        fout << a << "\n";
+    }
+    fin.close();
+    fout.close();
+    */
 
+  fs fstream fs;
+  fs.open (path, fstream::in | fstream::out | fstream::app);
+  if (!fs.is_open())
+  {
+      cout << "Opening myFile error!" << endl;
+  }
+  else
+      {
+      string msg;
+      int value;
+      cout << "MyFile was successfully opened!" << endl;
+      cout << "Press 0 for saving entered numbers to file. " << endl;
+      cout << "Press 1 for reading all numbers from file. " << endl;
+      cin >> value;
+
+      if (value == 0)
+      {
+          cin >> msg;
+          fs << msg<< "\n";
+
+      }
+      if (value == 1)
+      {
+          cout << "myFile consists of: " << endl;
+          while (!fs.eof())
+          {
+              msg ="";
+              fs >> msg;
+              cout << msg << endl;
+
+          }
+      }
+
+  }
+    fs.close();
     return 0;
 }
