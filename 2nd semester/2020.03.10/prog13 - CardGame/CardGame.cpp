bb8a15be02322721ac204cd0d89bc2f738a9e9f0
ZarinaMaks <55331028+ZarinaMaks@users.noreply.github.com>
2020-05-26 20:39:13 +0300

    cardgame edit

--- 2nd semester/2020.03.10/prog13 - CardGame/CardGame.cpp
+++ 2nd semester/2020.03.10/prog13 - CardGame/CardGame.cpp
@@ -5,84 +5,251 @@
 #include <iostream>
 #include <iterator>
 #include <map>
-#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <cstdlib>
 
+CardGame::CardGame() {
+    Reset();
+    CardEffect();
+    EnchantmentEffect();
+}
+int CardGame::GetCurrentTurn() const {
+    return CardGame::MyCurrentTurn;
 
-CardGame::CardGame() { Reset();}
-int getRandomNumber(int min, int max)
-{
-    static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
-    return static_cast<int>(rand() * fraction * (max - min + 1) + min);
 }
-int CardGame::GetMaxTurns() const
-{
-   MaxTurns = getRandomNumber(1,5);
-   return MaxTurns;
-}
-int CardGame::GetCurrentTurn() const { return CardGame::MyCurrentTurn; }
-void CardGame::Reset()
-{
+void CardGame::Reset() {
     MyCurrentTurn = 1;
     return;
 }
+void CardGame::CardEffect() {
+    CardEffectM.insert(make_pair("red attack", -4));
+    CardEffectM.insert(make_pair("green attack", -3));
+    CardEffectM.insert(make_pair("write attack", -1));
+    CardEffectM.insert(make_pair("black attack", -10));
+    CardEffectM.insert(make_pair("enchantment", 5));
+    CardEffectM.insert(make_pair("heal", 2));
 
-//TODO HPCounter
-/*bool CardGame::IsGameWon(string Command) const
-{
-    if (Command == MyHiddenWord)
-    {
-        std::cout << "Congratulations! You won!\n";
-        return true;
-    }
-    return false;
-}*/
+}
+void CardGame::EnchantmentEffect() {
+    EnchantmentEffectM.insert(make_pair("enchantment", 5));
+}
+void CardGame::ResetTurnNumber() {
+    MyCurrentTurn = 1;
 
-EWordStatus CardGame::CheckCommandValidity(string Command) const
-{
-    if (!IsCommand(Command)) //if the word isn't an command, return the error Wrong_Word:
-    {
-        return EWordStatus::Wrong_Word;
-    }
-    else if (/*TODO CHECK IF IT IS A CARD PLAYER HAS*/) //if the command tries to get a card which a player does not have, return the error No_Card:
-    {
-        return EWordStatus::No_Card;
-    }
-    else  //otherwise, return OK:
-    {
-        return EWordStatus::OK;
-    }
-    //return EWordStatus::OK;
+}
+
+Player::Player(map<string, int> deck, int hpcount) {
+    Deck = deck;
+    HPcount = hpcount;
 }
 
 
-//reseives a VALID command, incriments turns, returns HPCount
-HPCount CardGame::SubmitValidCommand(string Command)
-{
-    MyCurrentTurn++;
-    HPCount HPCount;  //setup a return variable
-    int WordLength = MyCommand.length(); //assuming same length as command
+void Player::DisplayDeck() {
+    //TODO display deck by using iterator map key and value pairs
+}
 
-    if (/*TODO*/)
+bool Player::PlayCard(string Card) {
+
+    //Check if exists:
+    if (Deck.count(Card) == 0)
     {
-        //TODO for the amound of damage:
-        HPCount.HP_Player1--;
+        cout << "UPS! You don't have such card - enter a valid card name!" << endl;
+        return false;
     }
-    else
+
+    Deck[Card]--;
+    if (Deck[Card] <= 0)
+    {
+        Deck.erase(Card);
+    }
+    return true;
+
+}
+
+void Player::AddMoreCards(map<string, int> newdeck) {
+    // Create a map iterator and point to beginning of map
+    map<string, int>::iterator it = newdeck.begin();
+
+    // Iterate over the map using Iterator till end.
+    while (it != newdeck.end())
+    {
+        // Accessing KEY from element pointed by it.
+        string card = it->first;
+
+        // Accessing VALUE from element pointed by it.
+        int count = it->second;
+        //Check if exists
+        if (Deck.count(card) == 0)
         {
-        //TODO for the amound of damage:
-        HPCount.HP_Player2--;
+            Deck.insert(make_pair(card, count));
+        } else {
+            Deck[card] += count;
         }
+        //cout << card << endl;
+        // Increment the Iterator to point to next entry
+        it++;
     }
+}
+
+int Player::ChangeHPCount(int effect){
+    HPCount += effect;
     return HPCount;
 }
 
-void CardGame::ResetTurnNumber() { MyCurrentTurn = 1; }
+int CardGame::GetCardEffect(string Command) {
 
-bool CardGame::IsCommand(string Word) const
-{
-    /*TODO COMPARE TO WHAT IS IN CommandBar*/
-    return true;
+    //Check if exists
+    if (CardEffectM.count(Command) == 0)
+    {
+        return 0;
+    }
+    return CardEffectM[Command];
+
 }
+/*int CardGame::GetEnchantmentEffect(string Command) {
+
+  //Check if exists
+  if (EnchantmentEffectM.count(Command) == 0)
+	{
+	    return 0;
+	}
+	return EnchantmentEffectM[Command];
+
+}*/
+
+string CardGame::GetRandomCard() {
+
+    auto it = CardEffectM.begin();
+    advance(it, rand() % CardEffectM.size());
+    string random_key = it->first;
+    return random_key;
+}
+
+
+map<string, int> CardGame::CreateDisplayDeck() {
+    /*
+   int j;
+   cout << endl;
+   Card Deck_1Player [ 5 ];
+   Card Deck_2Player [ 5 ];
+
+   // create 2 Decks
+   for ( j = 0; j < 5; j++ )
+   {
+       int num = ( j % 7 ) + 2;
+       Suit su = Suit ( j / 7 );
+       Deck_1Player [ j ].set ( num, su );
+       Deck_2Player [ j ].set ( num, su );
+   }
+
+   // displaying 2 started Decks
+   srand ( time ( NULL ) );
+
+   //----Player 1----//
+   cout << "1st Player started Deck:\n";
+   for ( j = 0; j < 5; j++ )
+   {
+       Deck_1Player [ j ].display ( );
+       cout < "  ";
+
+       // start new line after Player1 5-Card-Deck
+       if ( !( ( j + 1 ) % 5 ) )
+           cout << endl;
+   }
+
+   for ( j = 0; j < 5; j++ )
+   {
+       int k = rand ( ) % 5;     // take random Card
+       Card temp1 = Deck_1Player [ j ];    // swap it with the current
+       Deck_1Player [ j ] = Deck_1Player [ k ];
+       Deck_1Player [ k ] = temp1;
+   }
+   // displaying mixed Deck
+   cout << "\nMixed 1st Player's Deck:\n";
+   for ( j = 0; j < 5; j++ )
+   {
+       Deck_1Player [ j ].display ( );
+       cout << "  ";
+       if ( !( ( j + 1 ) % 5 ) ) // start new line after each 5 Card
+           cout << endl;
+   }
+
+   //--------------//
+
+
+   //----Player 2----//
+   cout << "2nd Player started Deck:\n";
+   for ( j = 0; j < 5; j++ )
+   {
+       Deck_2Player [ j ].display ( );
+       cout < "  ";
+
+       // start new line after Player2 5-Card-Deck
+       if ( !( ( j + 1 ) % 5 ) )
+           cout << endl;
+   }
+
+   for ( j = 0; j < 5; j++ )
+   {
+       int k = rand ( ) % 5;     // take random Card
+       Card temp2 = Deck_2Player [ j ];    // swap it with the current
+       Deck_2Player [ j ] = Deck_2Player [ k ];
+       Deck_2Player [ k ] = temp2;
+   }
+   // displaying mixed Deck
+   cout << "\nMixed 2nd Player's Deck:\n";
+   for ( j = 0; j < 5; j++ )
+   {
+       Deck_2Player [ j ].display ( );
+       cout << "  ";
+       if ( !( ( j + 1 ) % 5 ) ) // start new line after each 5 Card
+           cout << endl;
+   }
+   //--------------//
+   */
+    map<string, int> Deck_Player;
+    for (i = 0; i < 5; i++) {
+        auto card = CGame.GetRandomCard();
+        //auto effect = CGame.GetCardEffect(card);
+
+        //Check if exists:
+        if (Deck.count(Card) == 0) {
+            Deck_Player.insert(make_pair(card, 1));
+        } else {
+            Deck_Player[card]++;
+        }
+        cout << card << "; ";
+    }
+    return Deck_Player;
+}
+
+/*void CardGame::CreateDisplay2Deck() {
+
+   cout << "2nd Player Cards: " << endl;
+   for (i = 0; i < 5; i++) {
+      auto card = CGame.GetRandomCard();
+      auto effect = CGame.GetCardEffect(card);
+      Deck_2Player.insert(make_pair(card, effect));
+      cout << card << "; ";
+   }
+
+}*/
+
+//TODO CheckCommandValidity
+/*CommandStatus CardGame::CheckCommandValidity(string Command) const
+{
+   if (!IsCommand(Command)) //if the word isn't an command, return the error Wrong_Word:
+   {
+       return CommandStatus::Wrong_Word;
+   }
+   else if (//TODO CHECK IF IT IS A CARD PLAYER HAS) //if the command tries to get a card which a player does not have, return the error No_Card:
+   {
+       return CommandStatus::No_Card;
+   }
+   else  //otherwise, return OK:
+   {
+       return CommandStatus::OK;
+   }
+   //return CommandStatus::OK;
+}*/
--- 2nd semester/2020.03.10/prog13 - CardGame/main.cpp
+++ 2nd semester/2020.03.10/prog13 - CardGame/main.cpp
@@ -1,9 +1,32 @@
 /*
- * (10-40 баллов) Написать программу для настольной карточной игры (в текстовой форме).
+(10-40 баллов) Написать программу для настольной карточной игры (в текстовой форме).
 - Игрок выбирает действия (2-3 типа)
 - 4-5 видов карт (с возможностью развития)
 - Взаимодействие между игроками определяется наборами карт, модификаторами, случайными событиями
- */
+
+Game Design:
+2 players (11 HP in the beginning)
+4 types(suits) of Cards - red, green, white, black
+3 types of action Cards:
+attack  - damaging a player
+red damage = 3, green damage = 2, white damage = 1, black damage = 10
+heal - raise 2 HP of a player
+enchantment - take 5 Cards more
+
+Game Steps:
+Player 1 gets the 5 Card Deck
+Player 2 gets the 5 Card Deck
+Turn 1: Player 1 uses 1 Card
+Turn 1: Player 2 uses 1 Card
+Turn 2: Player 1 uses 1 Card
+Turn 2: Player 2 uses 1 Card
+End of game: Winner - a player with the biggest amount of HP
+
+More Detailed Desccription of Game Steps:
+STEPS 1-2: display randomized 5 Card Deck two times
+STEP 3-6: player should use only Cards they got on the 1 step and they enter “red attack”, “green attack”,  “white attack”, “black attack”, “heal”, “enchantment” in other cases a player gets an ERROR
+STEP 6-7: displaying HP of each player
+*/
 
 #include <iostream>
 #include <cstdlib>
@@ -14,178 +37,91 @@
 #include <stdlib.h>
 using namespace std;
 
-
-// Function prototypes as outside a class, which we re-use across plays
+// Function prototypes as outside a class, which we re-use across plays:
 void PrintIntro();
 void PlayGame();
 string GetValidCommand();
 bool AskToPlayAgain();
-CardGame CGame; // instantiate a new game
+void CreateDisplayDeck();
+// instantiate a new game:
+CardGame CGame;
 
-// Type Cards
-// type 1 - red cards, type 2 - green cards
-// type 3 - white cards, type 4 - black cards
-enum Suit { type1, type2, type3, type4 };
-const int Card1 = 1; //attacking card - action 1
-const int Card2 = 2; //healing card - action 2
-const int Card3 = 3; //enchantment - action 3
+/* Type Cards:
+  type 1 - red Cards, type 2 - green Cards
+  type 3 - white Cards, type 4 - black Cards
+  type 5 - heal Cards, type 6 - enchantment Cards
+  */
 
-void PrintIntro()
-{
-    cout << "Welcome to Magic The Colouring, a fun card game!\n";
+
+//Checked:
+void PrintIntro() {
+    cout << "Welcome to Magic The Colouring, a fun Card game!\n";
     cout << endl;
-    cout << "         _             _                                  _            " << endl;
-    cout << "  _ __  | | __ _ _   _(_)_ __   __ _     ___ __ _ _ __ __| |___        " << endl;
-    cout << " | '_ \\| |/ _`  | | | | | '_ \\/ _` |  / __/ _` | '__/ _` / __|       " << endl;
-    cout << " | |_)  | | (_|  | |_| | | | | | (_| | (_| (_| | | | (_| \\__ \\       " << endl;
-    cout << " | .__/ |_|\\__,_|\\__ |_|_| |_|\\__,| \\___\\_,_|_| \\__,_|___/       " << endl;
-    cout << " |_|              |___/          |___/                                 " << endl;
-    cout << "                       MAGIC THE COLOURING                             " << endl;
-    cout << "                                                                       " << endl;
-    cout << "                                                                       " << endl;
-    cout << "                       3 types of action cards:                        " << endl;
-    cout << "          1) ATTACK  - damaging an opponent:                           " << endl;
-    cout << "              red attack = 3 pts, green damage = 2 pts                 " << endl;
-    cout << "              white damage = 1pts, black damage = 10pts                " << endl;
-    cout << "          2) HEAL - raise 2 HP                                         " << endl;
-    cout << "          3) ENCHANTMENT - take 5 cards more                           " << endl;
-    cout << "                                                                       " << endl;
+    cout << "         _              _                                 _       " << endl;
+    cout << "  _ __  | | __ __ _   _(_)_ __   __ _    ___ __ _ _ __ __| |___   " << endl;
+    cout << " | '_ \\| |/ _`  | | | | | '_ \\/ _` |  / __/ _` | '__/ _` / __|  " << endl;
+    cout << " | |_)  | | (_|  | |_| | | | | | (_| | (_| (_| | | | (_| \\|__\\  " << endl;
+    cout << " | .__/ |_|\\__,_|\\__ |_|_| |_|\\__,| \\___\\_,_|_| \\__,_|___/  " << endl;
+    cout << " |_|              |___/          |___/                            " << endl;
+    cout << "                       MAGIC THE COLOURING                        " << endl;
+    cout << "                                                                  " << endl;
+    cout << "                                                                  " << endl;
+    cout << "                       3 types of action Cards:                   " << endl;
+    cout << "          1) ATTACK  - damaging an opponent:                      " << endl;
+    cout << "              1 - red attack = 4 pts, 2 - green damage = 3 pts    " << endl;
+    cout << "              3 - white damage = 1 pts, 4 - black damage = 10pts  " << endl;
+    cout << "          2) 5 - HEAL - raise 2 HP                                " << endl;
+    cout << "          3) 6 - ENCHANTMENT - take 5 Cards more                  " << endl;
+    cout << "                           7 - EXIT GAME                          " << endl;
 
     return;
 }
-class Card
-{
-private:
-    int number;
-    Suit suit;
-public:
-    card ( ) // constructor
-    { }
-    void set ( int n, Suit s ) // setting the value
-    { suit = s; number = n; }
-    void display ( );          // displaying the card
-};
-void card::display ( )
-{
-    if ( number >= 1 && number <= 3 )
-        cout << number;
-    else
-    {
-        switch ( number )
-        {
-            case Card1: cout << 'Attack'; break;
-            case Card2: cout << 'Heal'; break;
-            case Card3: cout << 'Enchantment'; break;
-        }
-    }
-    switch ( suit )
-    {
-        case type1: cout << static_cast<char>( 4 ); break;
-        case type2: cout << static_cast<char>( 5 ); break;
-        case type3: cout << static_cast<char>( 6 ); break;
-        case type4: cout << static_cast<char>( 7 ); break;
-    }
-}
 
-
-int main ( )
-{
-    do {
-        PlayGame();
-        CGame.ResetTryNumber();
-        //TODO ResetTryNumber
-    }
-    while (AskToPlayAgain());
-    return 0;
-}
-
-void PlayGame()
-{
-    PrintIntro();
-    card deck [ 15 ];
-    int j;
-
-    cout << endl;
-
-    // create a deck
-    for ( j = 0; j < 15; j++ )
-    {
-        int num = ( j % 5 ) + 2;
-        Suit su = Suit ( j / 5 );
-        deck [ j ].set ( num, su );
-    }
-
-    // displaying started deck
-    cout << "Your started deck:\n";
-    for ( j = 0; j < 15; j++ )
-    {
-        deck [ j ].display ( );
-        cout < "  ";
-        if ( !( ( j + 1 ) % 5 ) ) // start new line after each 5 card
-            cout << endl;
-    }
-
-    // analyze generator of random numbers
-    srand ( time ( NULL ) );
-    for ( j = 0; j < 15; j++ )
-    {
-        int k = rand ( ) % 15;     // take random card
-        card temp = deck [ j ];    // swap it with the current
-        deck [ j ] = deck [ k ];
-        deck [ k ] = temp;
-    }
-
-    // displaying mixed deck
-    cout << "\nMixed deck:\n";
-    for ( j = 0; j < 15; j++ )
-    {
-        deck [ j ].display ( );
-        cout << "  ";
-        if ( !( ( j + 1 ) % 5 ) ) // start new line after each 5 card
-            cout << endl;
-    }
-    // TODO write each card's effect
-}
-
-//loop continually until the user gives a valid command
-string GetValidCommand()
-{
-    string Command = "";
-    EWordStatus Status = EWordStatus::Invalid_Status;
-
-    do {
-        int CurrentTurn = CGame.GetCurrentTurn();
-
-        // get a command from the player
-        cout << "Turn " << CurrentTurn << " of " << CGame.GetMaxTurn() << ".( ͡° ͜ʖ ͡°)Enter your command: ";
-        getline(cin, Command);
-
-        //check status, give feedback
-        Status = CGame.CheckCommandValidity(Command);
-        switch (Status)
-        {
-                //TODO write Wrong_Word
-            case EWordStatus::Wrong_Word:
-                cout << "Please enter a COMMAND.\n";
-                break;
-
-                //TODO write No_Card
-            case EWordStatus::No_Card:
-                cout << "Please enter a card which YOU HAVE.\n";
-                break;
-
-            default:
-                return Command;
-                break;
-        }
-    } while (Status != EWordStatus::OK); //keep looping until we get no errors
-    return Command;
-}
-
-bool AskToPlayAgain()
-{
+bool AskToPlayAgain() {
     cout << "Do you want to play again(y/n)? ";
     string Response = "";
     getline(cin, Response);
     return (Response[0] == 'y') || (Response[0] == 'Y');
 }
+
+int main ( ) {
+    do {
+        PlayGame();
+        CGame.ResetTryNumber(); //TODO ResetTryNumber
+    }
+    while (AskToPlayAgain());
+    return 0;
+}
+
+//Non-checked:
+void PlayGame() {
+    int NumberOfTurns = 2;
+    PrintIntro();
+
+    auto Player1 = new Player(CGame::CreateDisplayDeck(), 11);
+    auto Player2 = new Player(CGame::CreateDisplayDeck(), 11);
+
+    while (NumberOfTurns > 0) {
+        cout << "Player 1, your turn, this is your deck: " << endl;
+        // Display Deck
+        cout << "Player 1, enter your command: " << endl;
+        cin >> Command;
+        // Play Card() <- while PlayCard false display error
+        // Apply Effect
+
+        cout << "Player 2, your turn, this is your deck: " << endl;
+        //"Player 2, your turn, this is your deck"
+        // Display Deck
+        cout << "Player 2, enter your command: " << endl;
+        cin >> Command;
+        // Play Card() <- while PlayCard false display error
+        // Apply Effect
+
+        //NumberOfTurns--
+
+    }
+    //TODO Player::GetHPCount()
+    //compare Player1.HPCount with Player2.HPCount
+}
+
+//TODO GetValidCommand
